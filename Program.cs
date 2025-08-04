c2661bd8fed1946128eed2b709de62739d9a0c72
Maxwell Kpetsey <136545967+emkay3251@users.noreply.github.com>
2025-08-04 16:10:54 +0000

    ABSTRACT CLASSES & METHODS

--- Program.cs
+++ Program.cs
@@ -26,3 +26,42 @@ namespace OOPConceptsDemo
             Console.WriteLine("Meow");
         }
     }
+
+
+    // ABSTRACT CLASSES & METHODS 
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
